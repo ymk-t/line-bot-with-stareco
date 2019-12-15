6f4f375db69e43e7aaf935b77bad1e0e8f87733e
Yumeki <yumekui64@gmail.com>
2019-12-15 12:08:21 +0900

    test

--- index.js
+++ index.js
@@ -32,7 +32,9 @@ server.post('/bot/webhook', line.middleware(line_config), (req, res, next) => {
     req.body.events.forEach((event) => {
         // この処理の対象をイベントタイプがメッセージで、かつ、テキストタイプだった場合に限定。
         if (event.type == "message" && event.message.type == "text"){
-            const result = map.callMap(event.message.text)
+            const result = map.callMap(event.message.text).then(() => {
+                console.log('OK')
+            });
             
             console.log(event.message.text)
             console.log(result)
