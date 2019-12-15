da0ec3476b35a206c5823c02cfc976a3526b4946
Yumeki <yumekui64@gmail.com>
2019-12-15 11:44:30 +0900

    change map.js

--- map.js
+++ map.js
@@ -1,15 +1,19 @@
 const axios = require('axios')
-require('dotenv').config()
 
 exports.callMap = function (text) {
-  const response = axios.get(
+  const result = searchStarbucks(text)
+  return result
+}
+
+async function searchStarbucks (text) {
+  const response = await axios.get(
     'https://maps.googleapis.com/maps/api/place/findplacefromtext/json',
     {
       method: 'get',
       params: {
         language: 'ja',
         fields: 'formatted_address,name,place_id,photos',
-        input: text,
+        input: text + ' スターバックス',
         inputtype: 'textquery',
         key: process.env.GOOGLE_MAP_API
       }
